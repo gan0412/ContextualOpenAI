2d2686591cd89d00cef38fe375417da19d5f0dfe
Ganesh Rayavarapu <116599817+gan0412@users.noreply.github.com>
2025-06-04 14:35:10 -0400

    one more slight edit to bash script

--- context.sh
+++ context.sh
@@ -1,12 +1,7 @@
 git clone --depth 1 --filter=blob:none --sparse https://github.com/gan0412/ContextualOpenAI.git temp_ContextualOpenAI
 cd temp_ContextualOpenAI
 git sparse-checkout set context.py chistory.py
-
-# Move the files to the current directory
 mv context.py chistory.py ..
-
-# Go back to the parent directory and remove the temporary clone directory
 cd ..
 rm -rf temp_ContextualOpenAI
-
 echo "Files have been copied to the current directory."
